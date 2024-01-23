5d1382f10a8bcd1b9b2cda73206a7082217b21bb
xel26 <zhxo111nvshq@gmail.com>
2024-01-23 17:09:11 +0700

    update

--- main.go
+++ main.go
@@ -1,16 +1,18 @@
 package main
 
-// import "fmt"
+import "fmt"
+
+
 
 
 func main() {
 	// task 1
-	// params := [5]float64{4.37, 4.32, 4.324, 4.58, 4.1889}
+	params := [5]float64{4.37, 4.32, 4.324, 4.58, 4.1889}
 
-	// for _, v := range params{
-	// 	result := Pembulatan(v)
-	// 	fmt.Printf("%.2f\n", result)
-	// }
+	for _, v := range params{
+		result := Pembulatan(v)
+		fmt.Printf("%.2f\n", result)
+	}
 
 
 
