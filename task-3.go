52f8990f5185788ca632fc8032c08da07823a178
xel26 <zhxo111nvshq@gmail.com>
2024-05-14 14:55:08 +0700

    update task-3 interface

--- task-3.go
+++ task-3.go
@@ -28,19 +28,27 @@ func (p persegi) keliling() float64 {
 	return 4 * p.sisi
 }
 
-type kubus struct {
-	sisi float64
+type balok struct {
+	panjang float64
+	lebar float64
+	tinggi float64
 }
 
-func (p kubus) volume() float64 {
-	return p.sisi * p.sisi * p.sisi
+func (p balok) volume() float64 {
+	return p.panjang * p.lebar * p.tinggi
+}
+
+
+type CombinedStruct struct {
+	persegi
+	balok
 }
 
 func result() {
-	var bangunDatar hitung2d = persegi{6}
-	fmt.Println("luas Persegi: ", bangunDatar.luas())
-	fmt.Println("keliling Persegi: ", bangunDatar.keliling())
+	var persegi hitung = CombinedStruct{persegi: persegi{sisi: 2}}
+	var kubus hitung = CombinedStruct{balok: balok{panjang: 3, lebar: 2, tinggi: 1}}
 
-	var bangunRuang hitung3d = kubus{6}
-	fmt.Println("volume kubus: ", bangunRuang.volume())
+	fmt.Printf("keliling persegi %v ", persegi.keliling())
+	fmt.Printf("luas persegi %v ", persegi.luas())
+	fmt.Printf("volume balok %v ", kubus.volume())
 }
